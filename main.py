a08e73165f74de11410d6881b331931a675d7dd6
alphadzingina <alphaadx6@gmail.com>
2022-01-20 00:34:07 +0100

    welcome user and get total_bill

--- main.py
+++ main.py
@@ -2,3 +2,7 @@
 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
+
+print("Welcomee to the Tip Calculator!")
+print("\n")
+total_bill = input("What was the total bill?: ")
